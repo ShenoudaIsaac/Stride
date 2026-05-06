d9e41fd6bcf9d90367555af4c8fe8dc8bd7c2e6d
shenouda <shenouda@nweave.com>
2026-05-06 22:47:18 +0300

    feat-1/App Theme -Added colors in Color.kt -Handled themes due to the needs of project -Added dm_sans_medium.ttf, dm_sans_regular.ttf, sora_bold.ttf, sora_semibold.ttf, sora_regular.ttf to the project and use them in typoghraphy of the app

--- app/src/main/java/com/shenouda/stride/ui/theme/Color.kt
+++ app/src/main/java/com/shenouda/stride/ui/theme/Color.kt
@@ -1,11 +1,38 @@
 package com.shenouda.stride.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Primary
+val Primary = Color(0xFF005440)
+val OnPrimary = Color(0xFFFFFFFF)
+val PrimaryContainer = Color(0xFF0F6E56)
+val OnPrimaryContainer = Color(0xFF9AEDCF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Secondary
+val Secondary = Color(0xFF006C4E)
+val OnSecondary = Color(0xFFFFFFFF)
+val SecondaryContainer = Color(0xFF83F5C6)
+val OnSecondaryContainer = Color(0xFF007151)
+
+// Background & Surface
+val Background = Color(0xFFF7FAF6)
+val OnBackground = Color(0xFF181D1A)
+
+val Surface = Color(0xFFF7FAF6)
+val OnSurface = Color(0xFF181D1A)
+
+val SurfaceVariant = Color(0xFFE0E3E0)
+
+// Outline
+val Outline = Color(0xFF6F7A74)
+
+// Error
+val Error = Color(0xFFBA1A1A)
+val OnError = Color(0xFFFFFFFF)
+val PrimaryGradient = Brush.linearGradient(
+    colors = listOf(
+        Color(0xFF0F6E56),
+        Color(0xFF1D9E75)
+    )
+)
